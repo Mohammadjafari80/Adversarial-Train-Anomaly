34238154bf8338733528721adfedbd1451a716b6
Mohammad <mamad.jafari91@gmail.com>
2022-09-10 23:18:39 +0430

    implement model.py

---
+++ model.py
@@ -0,0 +1,29 @@
+from torchvision import models
+import torch
+import torch.nn as nn
+import torch.optim as optim
+
+class FeatureExtractor(nn.Module):
+  def __init__(self, model='18'):
+    super(FeatureExtractor, self).__init__()
+
+    # Load a pretrained resnet model from torchvision.models in Pytorch
+    self.model = None
+
+    if model == '18':
+        self.model = models.resnet18(pretrained=True)
+    else:
+        self.model = models.resnet152(pretrained=True)
+
+    num_ftrs = self.model.fc.in_features
+    self.model.fc = nn.Flatten()
+    self.head = nn.Linear(num_ftrs, 2)
+
+  def forward(self, x):
+    x = self.model(x)
+    x = self.head(x)
+    return x
+
+  def get_feature_vector(self, x):
+    x = self.model(x)
+    return x
