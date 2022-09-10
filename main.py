fix bugs and normalization issues

main.py
@@ -26,7 +26,7 @@ with open('./config.yaml') as f:
 batch_size = config['batch_size']
 normal_class = config['normal_class'] #@param ['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']                                      
 NUMBER_OF_EPOCHS = config['n_epochs']
-attack_params = config['n_epochs']
+attack_params = config['attack_params']
 AUC_EVERY = config['auc_every']
 
 if not os.path.exists(config['results_path']):
@@ -54,8 +54,7 @@ attack_params['model'] = model
 #######################
 
 transform = transforms.Compose([transforms.Resize((224, 224)),
-                                        transforms.ToTensor(),
-                                        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])])
+                                        transforms.ToTensor()])
 
 inv_normalize = transforms.Normalize(
     mean=[-0.485/0.229, -0.456/0.224, -0.406/0.255],
@@ -94,7 +93,7 @@ G = make_gan(gan_type='biggan', model_name='biggan-deep-128').to(device)
 
 attack = None
 
-if config['attachk_type'] == 'PGD':
+if config['attack_type'] == 'PGD':
     attack = PGD(**attack_params)
 else:
     attack = FGSM(**attack_params)
@@ -105,12 +104,12 @@ results = {}
 try:
     results = pd.read_csv(os.path.join(config['results_path'], f'{config["output_file_name"]}.csv')).to_dict(orient='list')
 except:
-    results['attack_config'] = []
-    results['label'] = []
-    results['auc_adv'] = []
-    results['sim_adv'] = []
-    results['auc_org'] = []
-    results['sim_org'] = []
+    results['Softmax AUC'] = []
+    results['Softmax Adversairal AUC'] = []
+    results['KNN AUC'] = []
+    results['KNN Adversairal AUC'] = []
+    results['Train Accuracy'] = []
+    results['Loss'] = []
 
 ##############
 #  Training  #
@@ -124,19 +123,24 @@ for epoch in range(NUMBER_OF_EPOCHS):
 
 
   if epoch % AUC_EVERY == 0 :
-      torch.cuda.empty_cache()
-      model.eval()
-      print('*' * 50)
-      auc = auc_softmax(model=model, epoch=epoch, test_loader=test_loader, device=device)
-      results["Softmax AUC"].append(auc * 100)
-      print('*' * 50)
-      adv_auc = auc_softmax_adversarial(model=model, epoch=epoch, test_loader=test_loader, attack=attack, device=device)
-      results["Softmax Adversairal AUC"].append(adv_auc * 100)
-      print('*' * 50)
-      auc, adv_auc = test_AUC(model=model, epoch=epoch, train_loader=train_loader, test_loader=test_loader, attack=attack, device=device)
-      results["KNN AUC"].append(auc * 100)
-      results["KNN Adversairal AUC"].append(adv_auc * 100)
-      print('*' * 50)
+        torch.cuda.empty_cache()
+        model.eval()
+        print('*' * 50)
+        auc = auc_softmax(model=model, epoch=epoch, test_loader=test_loader, device=device)
+        results["Softmax AUC"].append(auc * 100)
+        print('*' * 50)
+        adv_auc = auc_softmax_adversarial(model=model, epoch=epoch, test_loader=test_loader, attack=attack, device=device)
+        results["Softmax Adversairal AUC"].append(adv_auc * 100)
+        print('*' * 50)
+        auc, adv_auc = test_AUC(model=model, epoch=epoch, train_loader=train_loader, test_loader=test_loader, attack=attack, device=device)
+        results["KNN AUC"].append(auc * 100)
+        results["KNN Adversairal AUC"].append(adv_auc * 100)
+        print('*' * 50)
+  else:
+        results["Softmax AUC"].append('-')
+        results["Softmax Adversairal AUC"].append('-')
+        results["KNN AUC"].append('-')
+        results["KNN Adversairal AUC"].append('-')
 
   model.train()
   preds = []
@@ -162,7 +166,6 @@ for epoch in range(NUMBER_OF_EPOCHS):
        optimizer.step()
        tepoch.set_postfix(loss=running_loss / len(preds), accuracy=100. * accuracy)
 
-  data = inv_normalize(data)
   save_image(tensor=data, fp=os.path.join(config['results_path'], f'sample{epoch:03d}.png'), scale_each=True, normalize=True, nrow=4)
   results["Train Accuracy"].append(100. * accuracy)
   results["Loss"].append(running_loss / len(preds))

model.py
@@ -9,7 +9,7 @@ class FeatureExtractor(nn.Module):
 
     # Load a pretrained resnet model from torchvision.models in Pytorch
     self.model = None
-
+    self.norm = nn.BatchNorm2d(3)
     if model == 18:
         self.model = models.resnet18(pretrained=pretrained)
     else:
@@ -20,10 +20,12 @@ class FeatureExtractor(nn.Module):
     self.head = nn.Linear(num_ftrs, 2)
 
   def forward(self, x):
+    x = self.norm(x)
     x = self.model(x)
     x = self.head(x)
     return x
 
   def get_feature_vector(self, x):
+    x = self.norm(x)
     x = self.model(x)
     return x

utills.py
@@ -5,6 +5,11 @@ import torch
 from tqdm import tqdm
 import torchvision.transforms as transforms
 
+cifar10_mean = (0.4914, 0.4822, 0.4465)
+cifar10_std = (0.2471, 0.2435, 0.2616)
+
+mu = torch.tensor(cifar10_mean).view(3,1,1).cuda()
+std = torch.tensor(cifar10_std).view(3,1,1).cuda()
 
 def knn_score(train_set, test_set, n_neighbours=2):
     """
@@ -123,8 +128,7 @@ def get_data(model, G, data, target, attack, device):
     x = transforms.Resize((32, 32))(x)
     x = transforms.Resize((224, 224))(x)
 
-    fake_data = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])(x)
-    data = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])(data)
+    fake_data = (x - torch.min(x))/(torch.max(x)- torch.min(x))
     fake_target = torch.ones_like(target, device=device)
 
     new_data = torch.cat((fake_data, data))